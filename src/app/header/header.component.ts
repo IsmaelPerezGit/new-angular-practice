2e68652d94dea03211c63ed98d830b44a937fe95
Ismael Perez <ismaelperezemail@gmail.com>
2019-01-31 17:30:51 -0700

    -Removed hrefs from links and replaced with routerLinks

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,7 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html"
 })
-export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
-
-}
+export class HeaderComponent {}
